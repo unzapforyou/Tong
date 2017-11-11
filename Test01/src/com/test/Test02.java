310cb358a1ea95ae1e49a34e63b3b942a46f1b7f
tong <unzapforyou@naver.com>
2017-11-11 09:44:14 +0900

    웹페이지생성

---
+++ Test01/src/com/test/Test02.java
@@ -0,0 +1,11 @@
+package com.test;
+import com.test.testImpl;
+public class Test02 extends testImpl{
+
+	public static void main(String[] args) {
+	
+		String ar="진구진구";
+		System.out.println(ar);
+		System.out.println(v);
+	}
+}
---
+++ Test01/src/com/test/testImpl.java
@@ -0,0 +1,5 @@
+package com.test;
+
+public class testImpl {
+			String v="닭강정";
+}
